3d301b1c45740f8e67e1ad9af26989f470883b22
sevval <sevval.dagasann@gmail.com>
2022-09-28 22:18:03 +0300

    BolumRepository içine findByBolumName ve findAllByNAMELike metotları eklendi.

--- src/main/java/com/bilgeadam/SpringBootHibernateHbs/jparepository/BolumRepository.java
+++ src/main/java/com/bilgeadam/SpringBootHibernateHbs/jparepository/BolumRepository.java
@@ -1,6 +1,10 @@
 package com.bilgeadam.SpringBootHibernateHbs.jparepository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,5 +14,9 @@ import com.bilgeadam.SpringBootHibernateHbs.model.Bolum;
 @Transactional
 public interface BolumRepository extends JpaRepository<Bolum, Long>
 {
+	@Query(name = "findByKonuName", value = "SELECT * FROM public.bolum WHERE NAME LIKE :NAME", nativeQuery = true)
+	public List<Bolum> findByKonuName(@Param("NAME") String name);
+
+	public List<Bolum> findAllByNAMELike(String name);
 	
 }
